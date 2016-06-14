df5cd8bb506feb3eb1fec1e540104672989dad98
Brit Butler <redline6561@gmail.com>
2016-06-14 12:10:06 -0400

    Add some event listeners.

--- js/main.js
+++ js/main.js
@@ -21,3 +21,38 @@ var randomItem = function (items) {
   var index = Math.floor(Math.random() * items.length);
   return items[index];
 };
+
+var answer = randomItem(wordList);
+console.log("The magic word is " + answer);
+
+// addEventListener for js/page magic
+
+var bumpCount = function (e) {
+  var target = e.target;
+  target.innerHTML = Number(target.innerHTML) + 1;
+};
+
+var wipe = function (e) {
+  document.write("<div></div>");
+};
+
+var el = document.querySelector("#turn-count");
+el.addEventListener('click', bumpCount);
+
+var letters = document.querySelector(".alphabet");
+letters.addEventListener('click', function (e) { console.log(e); });
+
+
+// el.addEventListener('click', function (e) {
+//   var target = e.target;
+//   target.innerHTML = Number(target.innerHTML) + 1;
+// });
+
+
+// var target = null;
+// var el = document.querySelector("#turn-count");
+// el.addEventListener('click', function (e) {
+//   console.log(e);
+//   target = e.target;
+// });
+
