498251a881fbf37f0629ebc2c6400a87be669f8a
Brit Butler <redline6561@gmail.com>
2016-06-15 10:50:31 -0400

    Clean up the mess. Dead code elimination.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,3 @@
-// var sayHi = function (name) {
-//   console.log("Hello there, " + name);
-// };
-
-// console.log("Hangman is just a game.");
-// sayHi("Brit");
-
-// var addOne = function(x) {
-//   return x + 1;
-// };
-
-// document.querySelector("h1").addEventListener('click', function () {
-//   console.log(addOne(4));
-// });
-
 var turnsLeft = function () {
   var element = document.getElementById('turn-count');
   return element.textContent;
@@ -40,10 +25,6 @@ var bumpCount = function (event) {
   target.innerHTML = Number(target.innerHTML) + 1;
 };
 
-var wipe = function (event) {
-  document.write("<div></div>");
-};
-
 var el = document.querySelector("#turn-count");
 el.addEventListener('click', bumpCount);
 
@@ -56,35 +37,4 @@ letters.addEventListener('click', function (event) {
   } else {
     alert("You guessed that dummy.");
   }
-
-  // if (pageNode.classList.contains("guessed") == false) {
-  //   pageNode.classList.add("guessed");
-  //   guesses.push(pageNode.textContent);
-  // } else {
-  //   alert("You guessed that already. Dummy.");
-  // }
 });
-
-
-// el.addEventListener('click', function (e) {
-//   var target = e.target;
-//   target.innerHTML = Number(target.innerHTML) + 1;
-// });
-
-
-// var target = null;
-// var el = document.querySelector("#turn-count");
-// el.addEventListener('click', function (e) {
-//   console.log(e);
-//   target = e.target;
-// });
-
-// var butt = document.querySelector(".form-button");
-// butt.addEventListener('click', function (event) {
-//   event.preventDefault();
-//   var color = randomItem(["red", "blue", "green"]);
-//   var input = document.querySelector(".count-input");
-//   var turns = document.querySelector("#turn-count");
-//   turns.textContent = input.value;
-//   turns.style.backgroundColor = color;
-// });
